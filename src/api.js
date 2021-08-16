78c0ddc5f23e8123ee344ac8bb17cff749dffe9a
nagibinstudio <nagibinstudio@gmail.com>
2021-08-16 22:22:21 +0300

    fix bug

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ export const API_DOMEN = "https://api.iomp.ru/public/api";
 export const DOMEN = "https://api.iomp.ru";
 //http://127.0.0.1:8000/api
 //http://api.iomp.ru/
+// https://api.iomp.ru/public/api
 
 // CART_DOMEN
 export const CART_DOMEN = "https://shop.iomp.ru/?r=ordering/cart/s1&lg=ru";
